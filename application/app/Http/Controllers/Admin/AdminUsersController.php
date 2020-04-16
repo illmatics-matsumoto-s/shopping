1119139705d824695f6a45d58af73ab98d167a05
illmatics-matsumoto-s <matsumoto-s@illmatics.co.jp>
2020-04-16 18:30:35 +0900

    使用しないプロパティの削除

--- application/app/Http/Controllers/Admin/AdminUsersController.php
+++ application/app/Http/Controllers/Admin/AdminUsersController.php
@@ -3,31 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\Admin\AdminUserSearchRequest;
-use App\Services\Domain\Admin\AdminUserManagementService;
 use App\Services\Domain\Admin\AdminUserManagementSearchService;
 use App\Http\Controllers\Controller;
 
 class AdminUsersController extends Controller
 {
-    protected $adminUserManagement,$selectSorts,$selectSortKeys,$selectSortNums;
-
-    private $radioIsOwners;
-
-    /**
-     * AdminUsersController constructor.
-     * @param AdminUserManagementService $adminUserManagement 管理者管理サービス
-     */
-    public function __construct(AdminUserManagementService $adminUserManagement)
-    {
-        // 管理者管理サービス
-        $this->adminUserManagement = $adminUserManagement;
-        // 画面項目マスタのセット
-        $this->radioIsOwners  = $this->adminUserManagement::getRadioIsOwners();
-        $this->selectSorts    = $this->adminUserManagement::getSelectSorts();
-        $this->selectSortKeys = $this->adminUserManagement::getSelectSortKeys();
-        $this->selectSortNums = $this->adminUserManagement::getSelectSortNums();
-    }
-
     /**
      * Action:search
      *
@@ -40,10 +20,6 @@ class AdminUsersController extends Controller
         $request->validated();
 
         return view('admin.admin_users.index', [
-            'radioIsOwners' => $this->radioIsOwners,
-            'selectSortKeys' => $this->selectSortKeys,
-            'selectSorts' => $this->selectSorts,
-            'selectSortNums' => $this->selectSortNums,
             'adminUsers' => $adminUserManagementSearchService($request),
         ]);
     }
