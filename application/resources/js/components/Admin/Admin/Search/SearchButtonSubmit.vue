2f65b8da2d328e864b8a5f9add62cd078da6b839
illmatics-matsumoto-s <matsumoto-s@illmatics.co.jp>
2020-04-16 16:07:11 +0900

    管理者管理一覧画面の検索ボタンのコンポーネントを作成

---
+++ application/resources/js/components/Admin/Admin/Search/SearchButtonSubmit.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-md-2 mb-3">
+         <base-submit
+            v-bind:text=text
+            v-bind:classes=classes
+         ></base-submit>
+    </div>
+</template>
+
+<script>
+import BaseSubmit from "../../../BaseButtonSubmit";
+export default {
+    name: "SearchButtonSubmit",
+    components: {BaseSubmit},
+    data:function () {
+        return {
+            classes:[
+                'btn',
+                'btn-lg',
+                'btn-primary',
+                'btn-block',
+            ],
+            text:'検索'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ application/resources/js/components/BaseButtonSubmit.vue
@@ -0,0 +1,26 @@
+<template>
+    <button type="submit"
+            v-bind:class=classes
+    >
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "BaseButtonSubmit",
+    props:{
+        classes:{
+            type:Array,
+        },
+        text:{
+            type:String,
+            require:true,
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
